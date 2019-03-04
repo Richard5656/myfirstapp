166b91a56b2f613bdeec1a8ed82b067c2afd955a
Ricahrd56 <18270414620@139.com>
2019-03-05 00:02:52 +0800

    按钮的几种监听方式

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -3,9 +3,14 @@ package com.example.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //声明要用到的view对象
+    Button btn_test_onclicklistener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,19 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+
+        btn_test_onclicklistener = (Button) findViewById(R.id.btn_test_onclicklistener);
+        btn_test_onclicklistener.setOnClickListener(this);
+        //这里先findview，找出相应对象
+        //然后给对象设置监听者为自己，因为自己这个类实现了那个接口
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_test_onclicklistener:
+                btn_test_onclicklistener.setText("我被你点击了");
+        }
     }
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    Button btn_one;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_one = (Button) findViewById(R.id.btn_one);
+
     }
 
     /** Called when the user taps the Send button */
@@ -23,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void click_btn_one(View view) {
+        if (btn_one.getText() == "我被你点击了") {
+            btn_one.setText("按钮1");
+        } else {
+            btn_one.setText("我被你点击了");
+        }
     }
 
 }
