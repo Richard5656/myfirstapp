d4b7d55062d803a69bc7a48d6ee0c665880f27b7
Ricahrd56 <18270414620@139.com>
2019-03-08 19:54:36 +0800

    只是同步一下而已，heihei

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,17 +1,24 @@
 package com.example.myfirstapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayMessageActivity extends AppCompatActivity implements View.OnClickListener {
 
     //声明要用到的view对象
     Button btn_test_onclicklistener;
-
+    Button button_openbrowser;
+    RadioGroup radioGroup_one;
+    TextView textView_sexual;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +29,50 @@ public class DisplayMessageActivity extends AppCompatActivity implements View.On
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
+        final TextView textView = findViewById(R.id.textView);
         textView.setText(message);
 
-        btn_test_onclicklistener = (Button) findViewById(R.id.btn_test_onclicklistener);
-        btn_test_onclicklistener.setOnClickListener(this);
         //这里先findview，找出相应对象
-        //然后给对象设置监听者为自己，因为自己这个类实现了那个接口
+        btn_test_onclicklistener = (Button) findViewById(R.id.btn_test_onclicklistener);
+        button_openbrowser = (Button) findViewById(R.id.button_openbrowser);
+        radioGroup_one = (RadioGroup) findViewById(R.id.radio_button_group_one);
+        btn_test_onclicklistener.setOnClickListener(this);
+        button_openbrowser.setOnClickListener(this);
+        textView_sexual =(TextView) findViewById(R.id.textview_sexual);
+        radioGroup_one.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                        if (i == R.id.radio_button_one) {
+                            textView_sexual.setText("your sexual is man");
+                        } else {
+                            textView_sexual.setText("your sexual is women");
+                        }
+                    }
+                }
+        );
 
+        //然后给对象设置监听者为自己，因为自己这个类实现了那个接口
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("is downloading");
+        progressDialog.setMessage("please wait");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+
+        Toast.makeText(this,"aloha ",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.button_openbrowser:
+                Intent intent = new Intent();
+                intent.setAction("android.intent.action.VIEW");
+                intent.setData(Uri.parse("www.baidu.com"));
+                startActivity(intent);
             case R.id.btn_test_onclicklistener:
                 btn_test_onclicklistener.setText("我被你点击了");
         }
     }
+
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,21 +1,52 @@
 package com.example.myfirstapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     Button btn_one;
+    Button button_to_signin;
+    AlertDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_one = (Button) findViewById(R.id.btn_one);
 
+        // find my view
+        btn_one = (Button) findViewById(R.id.btn_one);
+        button_to_signin = (Button) findViewById(R.id.button_to_signin);
+
+        dialog = new AlertDialog.Builder(this).setTitle("my dialog")
+                .setMessage("sure to quit?")
+                .setPositiveButton("sure", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this,"haha", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                )
+                .setNegativeButton("no", null)
+                .create();
+        dialog.show();
+
+        button_to_signin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                button_to_signin.setText("clicked ");
+                Intent intent = new Intent(MainActivity. this, ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /** Called when the user taps the Send button */
